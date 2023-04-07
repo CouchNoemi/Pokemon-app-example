a20cdd686d4080e629a794e1d2fd48f9aee4f96a
NoemiCouch <Noemicouch@gmail.com>
2023-04-06 20:09:55 -0500

    minor

--- assets/JavaScript/Script.js
+++ assets/JavaScript/Script.js
@@ -2,24 +2,48 @@ let searchEl = document.getElementById("auto-complete");
 let searchBtn = document.getElementById("searchBtn");
 let dropItem = document.getElementById("dropItem");
 let pokemonSelect = document.getElementById("pokemonSelect");
+let allPokemonData = [];
 
-function setPokemonTypes() {
-  fetch("https://pokeapi.co/api/v2/type")
+function getAllPokemonData() {
+  let results = fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((res) => res.json())
     .then((data) => {
-      const { results } = data;
-
-      results.forEach((el) => {
-        var option = document.createElement("option"); // creating option element
-        option.textContent = el.name; // set text content of option element
-        option.value = el.name; // setting the value attribute of option element
-        pokemonSelect.appendChild(option); // add option element to pokemon select element
-      });
+      return data.results;
     })
     .catch((err) => console.log(err));
+
+  return results;
 }
 
+const setPokemonTypes = async () => {
+  const pokemonData = await getAllPokemonData();
+  pokemonData.forEach((el) => {
+    allPokemonData.push(el);
+    var option = document.createElement("option"); // creating option element
+    option.textContent = el.name; // set text content of option element
+    option.value = el.name; // setting the value attribute of option element
+    pokemonSelect.appendChild(option); // add option element to pokemon select element
+  });
+};
+
 setPokemonTypes();
+
+$(function () {
+  $("#auto-complete").autocomplete({
+    source: allPokemonData.map(({ name }) => name),
+  });
+});
+/*
+   <input
+              class="btn btn-dark mb-2 mt-2"
+  
+              id="auto-complete"
+              placeholder="Search the PokeDex"
+          />
+  
+          <button id='searchBtn' class="btn btn-dark text-white mb-2">Search</button>
+  
+  */
 
 // pokemons: https://pokeapi.co/api/v2/pokemon?limit=151
 // pokemon-types: https://pokeapi.co/api/v2/type
