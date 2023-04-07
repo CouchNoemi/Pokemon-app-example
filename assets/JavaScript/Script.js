2c121f791ca37b65d6ba1892dc044c3a1b084e36
NoemiCouch <Noemicouch@gmail.com>
2023-04-06 19:22:35 -0500

    minor

--- assets/JavaScript/Script.js
+++ assets/JavaScript/Script.js
@@ -23,3 +23,27 @@ setPokemonTypes();
 
 // pokemons: https://pokeapi.co/api/v2/pokemon?limit=151
 // pokemon-types: https://pokeapi.co/api/v2/type
+/*
+SEARCH BAR:
+dropDownBtn.addEventListener("click", getPokemons)
+//getAllPokemon();
+$(function() {
+  $('#auto-complete').autocomplete({
+    source: allPokemonData.map(({ name }) => name)
+  });
+});
+
+DROPDOWN:
+function getPokemons() {
+  allPokemonData.forEach(i => {
+    // console.log(i)
+    var li = document.createElement("li")
+    var a = document.createElement("a")
+    a.textContent = i.name
+    a.classList.add('dropdown-item')
+    a.href = '#'
+    li.appendChild(a)
+    dropItem.appendChild(li)
+  });
+}
+*/
